2cab5af502aac99a9a658bd0e3a24a16da9a47cf
Satwikloka <satwikloka321@gmail.com>
2025-05-14 23:45:47 +0530

    sithara-docs

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gds6TPtGEXHgqbTY5htQGwUpQCnAoPKLQ7hwUfju/aY=\"\n}"
---
+++ .next/static/chunks/app_layout_jsx_c0237562._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_97df3889._.js",
+  "static/chunks/node_modules_@headlessui_react_dist_b7f67f40._.js",
+  "static/chunks/node_modules_@floating-ui_react_dist_c1a64d8e._.js",
+  "static/chunks/node_modules_nextra_dist_01ffa40f._.js",
+  "static/chunks/node_modules_b39075e3._.js",
+  "static/chunks/node_modules_nextra-theme-docs_dist_style_c716097b.css"
+],
+    source: "dynamic"
+});
